86a58fd2d2a675acd72698e514f649b02185ba50
str4 <jarno.peuramaki@gmail.com>
2020-05-11 14:42:26 +0300

    Improved Sider Menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Layout, Menu } from "antd";
-import { Router, NavLink } from "react-router-dom";
+import { Layout } from "antd";
+import { Router  } from "react-router-dom";
 import makeMainRoutes from "./routes";
 
+import MainSider from "./components/MainSider";
 import TitleAnnouncer from "./components/TitleAnnouncer";
 
 import history from "./utils/history";
 
 const App = () => {
-  const { Header, Content, Sider, Footer } = Layout;
+  const { Header, Content, Footer } = Layout;
 
   const routes = makeMainRoutes();
 
@@ -18,21 +19,7 @@ const App = () => {
       <Layout>
         <Header className="header"></Header>
         <Layout>
-          <Sider width={200} className="site-layout-background">
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["sub1"]}
-              style={{ height: "100%", borderRight: 0 }}
-            >
-              <Menu.Item key="home">
-                <NavLink to="/">Home</NavLink>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <NavLink to="/settings">Settings</NavLink>
-              </Menu.Item>
-            </Menu>
-          </Sider>
+          <MainSider />
           <Layout style={{ padding: "0 24px 24px" }}>
             <div style={{ margin: "20px 0 20px 30px" }}>
               <TitleAnnouncer />
---
+++ src/components/MainSider.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import { Layout, Menu } from "antd";
+
+const MainSider = (props) => {
+  const { location } = props;
+  const { Sider } = Layout;
+  return (
+    <Sider width={200} className="site-layout-background">
+      <Menu
+        mode="inline"
+        style={{ height: "100%", borderRight: 0 }}
+        selectedKeys={[location.pathname]}
+      >
+        <Menu.Item key="/">
+          <NavLink to="/">Home</NavLink>
+        </Menu.Item>
+        <Menu.Item key="/settings">
+          <NavLink to="/settings">Settings</NavLink>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
+};
+
+export default withRouter(MainSider);
