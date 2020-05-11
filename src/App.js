06066663c8829ab309f5669ec3b4193b33e68f7e
str4 <jarno.peuramaki@gmail.com>
2020-05-11 11:23:41 +0300

    added layout skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Layout, Menu } from "antd";
 
-function App() {
+const App = () => {
+  const { Header, Content, Sider, Footer } = Layout;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header className="header">
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}></Menu>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{ height: "100%", borderRight: 0 }}
+          >
+            <Menu.Item key="1">Option 1</Menu.Item>
+            <Menu.Item key="2">Option 2</Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <div style={{ margin: "20px" }}>
+            <h2>Page title</h2>
+          </div>
+          <Content
+            className="site-layout-background"
+            style={{
+              background: "white",
+              padding: 24,
+              marginLeft: "30px",
+              marginRight: "30px",
+              minHeight: 780,
+            }}
+          >
+            Content
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            React, Mobx, Antd starter 2020
+          </Footer>
+        </Layout>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
